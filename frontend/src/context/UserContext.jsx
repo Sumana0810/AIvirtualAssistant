488668f9a90b07049805112c61a1bba21b530ee8
Sumana Mondal <sumanamondal329@gmail.com>
2026-03-11 13:52:40 +0530

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -4,7 +4,7 @@ import React, { createContext, useEffect, useState } from 'react'
 export const userDataContext = createContext()
 
 function UserContext({ children }) {
-    const serverUrl = "https://aivirtualassistant-backend-dgur.onrender.com"
+    const serverUrl = "https://ai-backend-0lxx.onrender.com"
     const [userData, setUserData] = useState(null)
     const [frontendImage, setFrontendImage] = useState(null)
     const [backendImage, setBackendImage] = useState(null)
