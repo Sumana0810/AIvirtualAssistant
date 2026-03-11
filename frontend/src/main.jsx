c197b2b6abd98b9a6d5d944e4bb74162efc84c55
Sumana Mondal <sumanamondal329@gmail.com>
2026-03-11 10:17:10 +0530

    fixed main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from "react-router-dom"
-<<<<<<< HEAD
+import { BrowserRouter } from "react-router-dom"
 import UserContext from './context/UserContext.jsx'
-=======
-import UserContext from './context/userContext.jsx'
->>>>>>> 1e71031c1869f6dea2fd69732c629e3451a1d9e6
+
 createRoot(document.getElementById('root')).render(
-<BrowserRouter>
-<UserContext>
-    <App />
-  </UserContext>
-  </BrowserRouter>
- 
+  <StrictMode>
+    <BrowserRouter>
+      <UserContext>
+        <App />
+      </UserContext>
+    </BrowserRouter>
+  </StrictMode>
 )
